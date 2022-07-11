1c9f339909637e8a33d2c8a16388d45292400cc1
TEL <yamax4160@icloud.com>
2022-07-11 16:31:30 +0900

    X、Y、ピカブイ等の単語も削除

--- get_csv.py
+++ get_csv.py
@@ -18,14 +18,12 @@ with open("zukan.csv","w",newline="", encoding='utf_8_sig') as zukan_csv_file:
     writer = csv.writer(zukan_csv_file)
 
 
-    i = 1
     time.sleep(2)
 
-    while i<979:
+    for i in range(1,979):
         send_button = driver.find_element(By.XPATH,f'//*[@id="mw-content-text"]/div/table[1]/tbody/tr[{str(i)}]/td[2]/a')
         try: #フォルム違いを排除
             driver.find_element(By.XPATH,f'//*[@id="mw-content-text"]/div/table[1]/tbody/tr[{str(i)}]/td[2]/small')
-            i+=1
             time.sleep(0.5)
             continue
         except:
@@ -33,19 +31,25 @@ with open("zukan.csv","w",newline="", encoding='utf_8_sig') as zukan_csv_file:
         pokemon_name = send_button.get_attribute("title") #ポケモン名取得
         print(pokemon_name)
         send_button.click() #送信ボタンを押す
-        poke_scripts = driver.find_elements(By.XPATH,'//*[@id="mw-content-text"]/div/dl[1]/dd')
+        poke_scripts = driver.find_elements(By.XPATH,'//*[@id="mw-content-text"]/div/dl[1]/dd') #図巻説明一覧取得
         candidate_list = []
         for scr in poke_scripts:
-            text_str = scr.text
+            text_str = scr.text #図巻説明文を取得
             if pokemon_name not in text_str and "(漢字)" in text_str: #(漢字)の文字が入っていて、そのポケモンの名前自体が入っていない図巻説明のみを取得
-                candidate_list.append(text_str.replace("(漢字)","")) #(漢字)を排除
+                text_str = text_str.replace("(漢字)","")
+                if "(X)" in text_str:
+                    text_str = text_str.replace("(X)","")
+                if "(Y)" in text_str:
+                    text_str = text_str.replace("(Y)","")
+                if "(ピカブイ)" in text_str:
+                    text_str = text_str.replace("(ピカブイ)","")
+                candidate_list.append(text_str) #(漢字)を排除
         
         for selected_text in random.sample(candidate_list,min(5,len(candidate_list))): #テキスト数は最大で5個まで
             print(selected_text)
             writer.writerow([selected_text,pokemon_name]) #csvに書き込み
         
         driver.back()
-        i+=1
         time.sleep(0.5)
 
     driver.save_screenshot(f"./test.png") #スクショ
